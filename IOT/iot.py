d44c9500fcf96c11dc432bf684a2161e684d01f9
Alexian A. <54548926+alexian-a@users.noreply.github.com>
2024-11-13 11:56:00 +0100

    Update iot.py

--- IOT/iot.py
+++ IOT/iot.py
@@ -12,6 +12,13 @@ topics = config['adress']['topics'].split(',')
 salles = set(config['donnees']['salles'].split(','))
 donnees = config['donnees']['donnees'].split(',')
 
+seuil = {}
+if (len(config['seuil']) != len(donnees)):
+    print("[WARNING] Seuil manquant dans le fichier de configuration")
+else:
+    for do in donnees:
+        seuil[do] = config['seuil'][do].split(',')
+
 def on_connect(client, userdata, flags, rc):
     print(f"Connecté avec le code de résultat {rc}")
     for topic in topics:
@@ -30,7 +37,12 @@ def on_message(client, userdata, msg):
             print(f"Salle -> {data[1]["room"]}")
             for do in donnees:
                 print(f"{do} : {data[0][do]}")
-
+                if float(data[0][do]) <= float(seuil[do][0]):
+                    print(f"[ALERT] Seuil minimum dépassé -> {do} : {data[0][do]}")
+                elif float(data[0][do]) >= float(seuil[do][1]):
+                    print(f"[ALERT] Seuil maximum dépassé -> {do} : {data[0][do]}")
+                else:
+                    print(f"{do} : {data[0][do]}")
 
 client = mqtt.Client()
 
