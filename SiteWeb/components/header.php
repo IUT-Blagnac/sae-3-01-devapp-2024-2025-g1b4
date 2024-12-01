ff59177e51ed10c7e34cfbc1bc200928fd3da80d
Alexian A. <54548926+alexian-a@users.noreply.github.com>
2024-12-01 18:13:44 +0100

    add site base

---
+++ SiteWeb/components/header.php
@@ -0,0 +1,27 @@
+<header>
+        <div class="header_title">
+            <p>La Roue Tourne</p>
+        </div>
+        <div class="header_navigation">
+            <a href="">
+                <span>Accueil</span>
+            </a>
+            <a href="">
+                <span>Nos boutiques</span>
+            </a>
+            <a href="">
+                <span>Acheter</span>
+            </a>
+        </div>
+        <div class="header_interaction">
+            <a href="">
+                <img src="./assets/accueil/Search.svg" alt="Profil" class="img_accueil">
+            </a>
+            <a href="">
+                <img src="./assets/accueil/Panier.png" alt="Profil" class="img_accueil">
+            </a>
+            <a href="">
+                <img src="./assets/accueil/Profil.png" alt="Profil" class="img_accueil">
+            </a>
+        </div>
+    </header>
