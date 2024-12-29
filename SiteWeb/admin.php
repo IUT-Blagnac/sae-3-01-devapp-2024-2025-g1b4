b14effde9d132696869acf4826c3d6327ed20fc5
alexian-a <alexian.arg11@gmail.com>
2024-12-29 23:27:12 +0100

    [US]: Créer un compte administrateur
    Closes #18 : Fermeture

--- SiteWeb/admin.php
+++ SiteWeb/admin.php
@@ -1,4 +1,21 @@
-<?php include './components/header.php' ?>
+<?php 
+
+    require_once './components/bd.php';
+
+    session_start();
+
+    $roleReq = $pdo->prepare("SELECT role FROM Client WHERE email=:mail");
+
+    $roleReq->execute(["mail" => $_SESSION["client_email"]]);
+    
+    $role = $roleReq->fetch();
+
+    if($role["role"]=='client')
+        header("Location: index.php?msg=".urlencode("Vous devez être administrateur!"));
+
+
+    include './components/header.php';
+    ?>
 
 <body>
     <div class="square-container1">
